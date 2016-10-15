43a3c8d38c20bf50f02f4566fdc4e1f2c4b986e4
David Mercer <radix42@gmail.com>
2016-10-14 18:45:58 -0700

    add build command to cross compile from linux to Windows

---
+++ zcutil/build-win.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+HOST="x86_64-w64-mingw32"
+
+set -eu -o pipefail
+
+if [ "x$*" = 'x--help' ]
+then
+    cat <<EOF
+Usage:
+
+$0 --help
+  Show this help message and exit.
+
+$0 [ --enable-lcov ] [ MAKEARGS... ]
+  Build Zcash and most of its transitive dependencies from
+  source. MAKEARGS are applied to both dependencies and Zcash itself. If
+  --enable-lcov is passed, Zcash is configured to add coverage
+  instrumentation, thus enabling "make cov" to work.
+EOF
+    exit 0
+fi
+
+set -x
+cd "$(dirname "$(readlink -f "$0")")/.."
+
+# If --enable-lcov is the first argument, enable lcov coverage support:
+LCOV_ARG=''
+HARDENING_ARG='--disable-hardening'
+if [ "x${1:-}" = 'x--enable-lcov' ]
+then
+    LCOV_ARG='--enable-lcov'
+    HARDENING_ARG='--disable-hardening'
+    shift
+fi
+
+# BUG: parameterize the platform/host directory:
+PREFIX="$(pwd)/depends/$HOST/"
+
+make HOST=$HOST "$@" -C ./depends/ V=1 NO_QT=1
+./autogen.sh
+./configure --prefix="${PREFIX}" --host=$HOST --with-gui=no "$HARDENING_ARG" "$LCOV_ARG" CXXFLAGS='-fwrapv -fno-strict-aliasing -Werror -g'
+make "$@" V=1
