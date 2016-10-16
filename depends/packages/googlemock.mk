feb42e7fd7754b15967925851802b2cd3800316f
David Mercer <radix42@gmail.com>
2016-10-16 02:11:37 -0700

    build google packages with gnu tools

--- depends/packages/googlemock.mk
+++ depends/packages/googlemock.mk
@@ -10,11 +10,11 @@ $(package)_download_file=release-$($(package)_version).tar.gz
 $(package)_sha256_hash=3f20b6acb37e5a98e8c4518165711e3e35d47deb6cdb5a4dd4566563b5efd232
 
 define $(package)_build_cmds
-  $(MAKE) -C make GTEST_DIR='$(host_prefix)' CXXFLAGS='-fPIC' gmock-all.o
+  CC=gcc-5 CXX=g++-5 AR=gar $(MAKE) -C make GTEST_DIR='$(host_prefix)' CXXFLAGS='-fPIC' gmock-all.o
 endef
 
 
 define $(package)_stage_cmds
-  install -D ./make/gmock-all.o $($(package)_staging_dir)$(host_prefix)/lib/libgmock.a && \
+  ginstall -D ./make/gmock-all.o $($(package)_staging_dir)$(host_prefix)/lib/libgmock.a && \
   cp -a ./include $($(package)_staging_dir)$(host_prefix)/include
 endef
--- depends/packages/googletest.mk
+++ depends/packages/googletest.mk
@@ -6,10 +6,10 @@ $(package)_download_file=release-$($(package)_version).tar.gz
 $(package)_sha256_hash=f73a6546fdf9fce9ff93a5015e0333a8af3062a152a9ad6bcb772c96687016cc
 
 define $(package)_build_cmds
-  $(MAKE) -C make CXXFLAGS=-fPIC gtest.a
+  CC=gcc-5 CXX=g++-5 AR=gar $(MAKE) -C make CXXFLAGS=-fPIC gtest.a
 endef
 
 define $(package)_stage_cmds
-  install -D ./make/gtest.a $($(package)_staging_dir)$(host_prefix)/lib/libgtest.a && \
+  ginstall -D ./make/gtest.a $($(package)_staging_dir)$(host_prefix)/lib/libgtest.a && \
   cp -a ./include $($(package)_staging_dir)$(host_prefix)/include
 endef
