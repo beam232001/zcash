513fc9c33e086420fbd4d291905e9ec1391a769a
David Mercer <radix42@gmail.com>
2016-10-16 01:52:15 -0700

    use local copy of profiling.cpp that is hacked to build on mac

--- depends/packages/libsnark.mk
+++ depends/packages/libsnark.mk
@@ -8,6 +8,10 @@ $(package)_git_commit=4d23a06660aa74a3f5be1bf4739bee0a37e05387
 
 $(package)_dependencies=libgmp libsodium
 
+define $(package)_preprocess_cmds
+  cp ~/Downloads/profiling.cpp src/common/
+endef
+
 define $(package)_build_cmds
   CXXFLAGS="-fPIC -DBINARY_OUTPUT -DNO_PT_COMPRESSION=1" $(MAKE) lib DEPINST=$(host_prefix) CURVE=ALT_BN128 MULTICORE=1 NO_PROCPS=1 NO_GTEST=1 NO_DOCS=1 STATIC=1 NO_SUPERCOP=1 FEATUREFLAGS=-DMONTGOMERY_OUTPUT
 endef
